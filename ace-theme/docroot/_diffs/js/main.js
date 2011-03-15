d4e62e5229ef0109daedcb6c96c6e1ff934b8412
gijze001 <gijze001@unknown.com>
2011-03-15 14:57:03 +0000

    Initial commit

---
+++ ace-theme/docroot/_diffs/js/main.js
@@ -0,0 +1,42 @@
+AUI().ready(
+	'liferay-hudcrumbs',
+	function(A) {
+		if (Liferay.Browser.isIe() && Liferay.Browser.getMajorVersion() < 7) {
+			var navigation = A.one('#navigation > ul');
+
+			if (navigation) {
+				navigation.delegate(
+					'mouseenter',
+					function(event) {
+						event.currentTarget.addClass('hover');
+					},
+					'> li'
+				);
+
+				navigation.delegate(
+					'mouseleave',
+					function(event) {
+						event.currentTarget.removeClass('hover');
+					},
+					'> li'
+				);
+			}
+		}
+
+		var siteBreadcrumbs = A.one('.site-breadcrumbs');
+
+		if (siteBreadcrumbs) {
+			siteBreadcrumbs.plug(A.Hudcrumbs);
+		}
+	}
+);
+
+//All JQuery stuff after this point
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function(){
+	$j("ul li:first-child").addClass("first");
+	
+    $j("ul li:last-child").addClass("last");
+});
