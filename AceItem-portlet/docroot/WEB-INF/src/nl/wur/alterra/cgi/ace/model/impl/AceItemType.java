e7eb962f98c5d48dcaab6baece25280a39d97ed6
Groot052 <Groot052@unknown.com>
2011-03-28 09:16:04 +0000

    Measure and Action separate

---
+++ AceItem-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/model/impl/AceItemType.java
@@ -0,0 +1,53 @@
+package nl.wur.alterra.cgi.ace.model.impl;
+
+/**
+ * Types of AceItems.
+ *
+ * @author heikki doeleman
+ */
+public enum AceItemType {
+    /**
+     * Document.
+     */
+    DOCUMENT,
+    /**
+     * Data (sets).
+     */
+    DATASET,
+    /**
+     * Tables.
+     */
+    TABLE,
+    /**
+     * Multimedia.
+     */
+    MULTIMEDIA,
+    /**
+     * Maps.
+     */
+    MAP,
+    /**
+     * Indicators.
+     */
+    INDICATOR,
+    /**
+     * Tool Guidance.
+     */
+    TOOLGUIDANCE,
+    /**
+     * ResearchProject.
+     */
+    RESEARCHPROJECT,
+    /**
+     * Measure.
+     */
+    MEASURE,
+    /**
+     * Action.
+     */
+    ACTION,
+    /**
+     * Organisation.
+     */
+    ORGANISATION
+}
