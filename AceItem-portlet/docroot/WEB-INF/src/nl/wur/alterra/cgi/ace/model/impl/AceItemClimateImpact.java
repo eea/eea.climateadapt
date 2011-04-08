ca9c8f7a6fcf196a02f9d3e9319813e762ccb080
Groot052 <Groot052@unknown.com>
2011-04-08 10:14:41 +0000

    Added enumeration ClimateImpacts

---
+++ AceItem-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/model/impl/AceItemClimateImpact.java
@@ -0,0 +1,33 @@
+package nl.wur.alterra.cgi.ace.model.impl;
+
+/**
+ * Types of ClimateImpacts.
+ *
+ * @author hugo de groot
+ */
+public enum AceItemClimateImpact {
+    /**
+     * Extreme Temperatures.
+     */
+    EXTREMETEMP,
+    /**
+     * Water Scarcity
+     */
+    WATERSCARCE,
+    /**
+     * Flooding
+     */
+    FLOODING,
+    /**
+     * Droughts
+     */
+    DROUGHT,
+    /**
+     * Storms
+     */
+    STORM,
+    /**
+     * Ice and Snow
+     */
+    ICEANDSNOW
+}
