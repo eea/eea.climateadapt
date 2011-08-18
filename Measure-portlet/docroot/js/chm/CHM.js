e7d6d25fb5af49ddea0b91f1f1fcab3cee57a2e2
vanme002 <vanme002@unknown.com>
2011-08-18 13:06:05 +0000

    opnieuw toevoegen

---
+++ Measure-portlet/docroot/js/chm/CHM.js
@@ -0,0 +1,11 @@
+/**
+ * @author vanme002
+ */
+(function() {
+    /**
+     * Namespace: OpenLayers
+     * The OpenLayers object provides a namespace for all things OpenLayers
+     */
+    window.CHM = {
+    };
+})();
