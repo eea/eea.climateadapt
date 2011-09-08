04db38f1744831a7013939959a997a28bebbe8e4
Groot052 <Groot052@unknown.com>
2011-09-08 13:55:26 +0000

    sql updated

--- Measure-portlet/sql/create_sat_view.sql
+++ Measure-portlet/sql/create_sat_view.sql
@@ -2,16 +2,16 @@ BEGIN;
 
 DELETE FROM public.geometry_columns WHERE f_table_name = 'casestudies' AND f_geometry_column = 'geom';
 
-CREATE OR REPLACE VIEW casestudies AS (
-	SELECT 
-		st_pointfromtext('POINT('|| lat || ' ' || lon ||')', 4326) AS geom, 
-		satarea AS area, 
-		"name", 
-		description,
-		website AS website,
-		sectors_ AS sectors,
-		climateimpacts_ AS risks
- 	FROM ace_measure);
+-- View: casestudies
+
+DROP VIEW casestudies;
+
+CREATE OR REPLACE VIEW casestudies AS 
+ SELECT st_pointfromtext(((('POINT('::text || ace_measure.lat) || ' '::text) || ace_measure.lon) || ')'::text, 4326) AS geom, ace_measure.satarea AS area, ace_measure.name AS itemname, "substring"(ace_measure.description, 1, 100) || ' ...'::text AS "desc", ace_measure.website, ace_measure.sectors_ AS sectors, ace_measure.climateimpacts_ AS risks, ace_measure.measureid
+   FROM ace_measure
+  WHERE ace_measure.mao_type::text = 'A'::text;
+
+ALTER TABLE casestudies OWNER TO postgres;
 
 INSERT INTO public.geometry_columns values ('', 'public', 'casestudies', 'geom', 2, 4326, 'POINT');
 
