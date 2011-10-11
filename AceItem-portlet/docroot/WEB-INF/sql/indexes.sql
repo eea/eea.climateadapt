2eb06dbe7260f1fd5dce1fbb7ffb1a6377583ff8
Groot052 <Groot052@unknown.com>
2011-10-11 09:09:16 +0000

    deleted WEB-INF/sql content. ALWAYS DO SO AFTER RUN SERVICEBUILDER

--- AceItem-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,8 +0,0 @@
-create index IX_9D82ED2A on Ace_AceItem (groupId);
-create index IX_FECA8C2A on Ace_AceItem (storedAt);
-
-create index IX_C0CB3E00 on Ace_NAS (groupId);
-
-create index IX_44BD51E5 on Ace_NASSource (groupId);
-
-create index IX_53EA24E4 on Ace_WxsHarvester (groupId);
