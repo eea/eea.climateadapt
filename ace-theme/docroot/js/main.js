c440331fe19c6799f9bfae623864ded62d0917de
gijze001 <gijze001@unknown.com>
2011-12-15 15:37:26 +0000

    Minor fixes to menu, styling contact page, minor additions to homepage, content titles not clickable anymore when not logged in.

--- ace-theme/docroot/js/main.js
+++ ace-theme/docroot/js/main.js
@@ -34,17 +34,22 @@ AUI().ready(
 //All JQuery stuff after this point
 
 $(document).ready(function(){
-	$("ul li:first-child").addClass("first");
 	
+	//Several classes for the main menu
+	$("ul li:first-child").addClass("first");
     $("ul li:last-child").addClass("last");
-    
     $('li').has('ul').addClass('submenu');
     
+    //Elements for the db search speedbutton on the frontpage
     $(".db-search-fp img").attr("src","/ace-theme/images/vergroot_blauw.png");
+    $('.db-search-fp .portlet-body').prepend('<p id="db-search-fp-info-text">Search the database</p>');
+    $('.db-search-fp .portlet-body').prepend('<a id="db-search-fp-link" href="/data-and-downloads"></a>');
     
+    //Fixes to main and sec. menu
     if ($("#sec-menu ul").length == 0){
-    	  $("#navigation .selected").css({"background":"none","border":"none"});
+    	  $("#navigation .selected").css({"background":"none","border":"none", "margin-top":"7px"});
     	  $("#topnav").css({"border-bottom":"1px solid #a5bf26"});
+    	  $("ul#topnav li.selected a").css({"color":"#FFF"});
     }
     
 });
