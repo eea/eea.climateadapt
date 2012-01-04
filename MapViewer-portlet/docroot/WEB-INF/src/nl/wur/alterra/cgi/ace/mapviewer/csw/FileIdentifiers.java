d0bae5aa1dff57d6be568b780cf3b705c43e9868
vanme002 <vanme002@unknown.com>
2012-01-04 16:37:51 +0000

    Reload layers added previously, username/password for GN

---
+++ MapViewer-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/mapviewer/csw/FileIdentifiers.java
@@ -0,0 +1,9 @@
+package nl.wur.alterra.cgi.ace.mapviewer.csw;
+
+import java.util.ArrayList;
+
+public class FileIdentifiers extends ArrayList<String> {
+
+	private static final long serialVersionUID = -2597819456192562953L;
+
+}
---
+++ MapViewer-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/mapviewer/csw/digitaltransferoption/DigitalTransferOptions.java
@@ -0,0 +1,12 @@
+package nl.wur.alterra.cgi.ace.mapviewer.csw.digitaltransferoption;
+
+import java.util.ArrayList;
+
+public class DigitalTransferOptions extends ArrayList<DigitalTransferOption> {
+
+	private static final long serialVersionUID = 1599074598785290747L;
+	
+	public DigitalTransferOptions() {
+		super();
+	}
+}
