275fc78cdca6364f3fd50a0067648c4ed7576969
Groot052 <Groot052@unknown.com>
2012-03-12 21:52:48 +0000

    Save as draft / Submit for publication

--- Project-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/Constants.java
+++ Project-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/Constants.java
@@ -9,9 +9,9 @@ public class Constants {
 
 	public static final String EDITURL = "editUrl"; // used inside ProjectGoEditPortlet
 
-	public static final short Status_SUBMITTED = -1; 
+	public static final short Status_SUBMITTED = 0; 
 	
-	public static final short Status_DRAFT = 0;
+	public static final short Status_DRAFT = -1;
 	
 	public static final short Status_APPROVED = 1;
 
