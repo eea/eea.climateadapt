30251abd309cd93869dfd558c3d33e0707323693
Groot052 <Groot052@unknown.com>
2012-03-13 15:29:56 +0000

    Save as draft / Submit for publication

--- AceSearchEngine/src/nl/wur/alterra/cgi/ace/search/lucene/ACEIndexUtil.java
+++ AceSearchEngine/src/nl/wur/alterra/cgi/ace/search/lucene/ACEIndexUtil.java
@@ -9,12 +9,24 @@ import java.util.Properties;
 /**
  * Utility methods for Lucene index in ACE.
  *
- * @author jose garcia
+ * @author jose garcia / hugo de Groot
  */
 public class ACEIndexUtil {
 	private static String indexFolderPath = "";
 	
 	private static int descriptionLength = 154;
+
+	private static String notificationHostUrl = "";
+
+	private static String notificationFromAddress = "";
+
+	private static String notificationToAddressList = "";
+
+	public static final short Status_SUBMITTED = 0; 
+	
+	public static final short Status_DRAFT = -1;
+	
+	public static final short Status_APPROVED = 1;
 	
 	/**
 	 * Reads the index folder from a properties file (if exists), otherwise works as before using relative paths
@@ -37,8 +49,14 @@ public class ACEIndexUtil {
    				(new File( path + "/aceindex.properties"));
 			
 			properties.load(fis);
-			
+
 			indexFolderPath = properties.getProperty("index.path", "");
+			
+			notificationHostUrl = properties.getProperty("notification.host.url", "");
+			
+			notificationFromAddress = properties.getProperty("notification.from.address", "");
+			
+			notificationToAddressList = properties.getProperty("notification.to.address.list", "");
 
 			try {
 				descriptionLength = Integer.parseInt(properties.getProperty("totaldescription.length", "154")) ;
@@ -46,7 +64,7 @@ public class ACEIndexUtil {
 			catch (Exception e) {
 				descriptionLength = 154;
 			}
-			
+
 			// Adds final File.separator
 			if (!indexFolderPath.equals("")) {
 				if (!indexFolderPath.endsWith(File.separator)) indexFolderPath += File.separator;
@@ -79,16 +97,41 @@ public class ACEIndexUtil {
 	}
 	
 	/**
-	 * Reads the index folder from a properties file (if exists)
+	 * Reads the description Length from a properties file (if exists)
 	 *
-	 * Create a file aceindex.properties in the same folder of AceSearchengine.jar with this property:
-	 *
-	 *		index.path=PATH
-	 *
-	 * @return Lucene index folder
+	 * @return description Length
 	 */
 	public static int retrieveTotalDescriptionLength() {
 		return descriptionLength;	
+	}	
+	
+	/**
+	 * Reads the notificationHostUrl from a properties file (if exists)
+	 *
+	 * @return notificationHostUrl
+	 */
+	public static String retrieveNotificationHostUrl() {
+		return notificationHostUrl;	
+	}	
+	
+	/**
+	 * Reads the notificationHostUrl from a properties file (if exists)
+
+	 *
+	 * @return description Length
+	 */
+	public static String retrieveNotificationFromAddress() {
+		return notificationFromAddress;	
+	}	
+	
+	/**
+	 * Reads the description Length from a properties file (if exists)
+	 *
+	 * @return description Length
+	 */
+	
+	public static String retrieveNotificationToAddressList() {
+		return notificationToAddressList;	
 	}
 	
 	/**
