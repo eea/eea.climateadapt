ca68bd7e159efda6c0714f4a5209007d71b80243
Groot052 <Groot052@unknown.com>
2012-03-15 12:28:49 +0000

    Only show approved Case studies

--- Measure-portlet/sql/create_sat_view.sql
+++ Measure-portlet/sql/create_sat_view.sql
@@ -9,15 +9,16 @@ DROP VIEW casestudies;
 CREATE OR REPLACE VIEW casestudies AS 
  SELECT st_pointfromtext(((('POINT('::text || ace_measure.lon) || ' '::text) || ace_measure.lat) || ')'::text, 4326) AS geom, ace_measure.satarea AS area, ace_measure.name AS itemname, "substring"(ace_measure.description, 1, 100) || ' ...'::text AS "desc", ace_measure.website, ace_measure.sectors_ AS sectors, ace_measure.climateimpacts_ AS risks, ace_measure.measureid
    FROM ace_measure
-  WHERE ace_measure.mao_type::text = 'A'::text
+  WHERE ace_measure.controlstatus = 1
+  AND ace_measure.mao_type::text = 'A'::text 
   AND ace_measure.lon IS NOT NULL 
-  AND ace_measure.lat IS NOT NULL 
-  AND ace_measure.lon <> 'NaN' 
-  AND ace_measure.lat <> 'NaN'  
-  AND ace_measure.lat > 0  ;
-
+  AND ace_measure.lat IS NOT NULL AND ace_measure.lon <> 'NaN'::double precision 
+  AND ace_measure.lat <> 'NaN'::double precision 
+  AND ace_measure.lat > 0::double precision;
 
 ALTER TABLE casestudies OWNER TO postgres;
+GRANT ALL ON TABLE casestudies TO postgres;
+GRANT SELECT ON TABLE casestudies TO chm_viewer;
 
 INSERT INTO public.geometry_columns values ('', 'public', 'casestudies', 'geom', 2, 4326, 'POINT');
 
