1aa4c38b3900ccc59fa0389bc3be54c550338d61
Hugo De Groot <hugo.degroot@wur.nl>
2012-10-30 12:05:12 +0000

    Users who add an item through 'Share your info' get an email-message that their contribution has been received and will be reviewed by a moderator.

--- AceItem-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/LuceneIndexUpdatePortlet.java
+++ AceItem-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/LuceneIndexUpdatePortlet.java
@@ -311,6 +311,14 @@ public abstract class LuceneIndexUpdatePortlet extends MVCPortlet {
     	body += "\nEmail: " + useremail;
     	
     	MailEngine.send(fromInternetAddress, toInternetAddresses, null, null, subject, body, false, null, null, null);
+
+      	// send second email to contributor
+    	InternetAddress[] contributorAddress = new InternetAddress[1];
+    	contributorAddress[0] = new InternetAddress(useremail);
+    	subject = "Your contribution to Climate-Adapt" ; 
+    	body = "Thank you for your contribution to Climate-Adapt regarding '" + aceitem.getName() + "'.\n\n" ; 
+    	body += "Your contribution will be reviewed and handled by the website moderators." ; 
+    	MailEngine.send(fromInternetAddress, contributorAddress, null, null, subject, body, false, null, null, null);
       }
       catch (Exception e) {
     	  // do nothing
