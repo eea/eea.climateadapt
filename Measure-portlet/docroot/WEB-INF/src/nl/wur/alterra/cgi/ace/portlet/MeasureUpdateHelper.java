d5d2846e6219829eac8b037ec8b786db38bd5c6d
Hugo De Groot <hugo.degroot@wur.nl>
2013-02-04 15:38:55 +0000

    Provide a link to the item in the submission notification email

--- Measure-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/MeasureUpdateHelper.java
+++ Measure-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/ace/portlet/MeasureUpdateHelper.java
@@ -353,7 +353,16 @@ public abstract class MeasureUpdateHelper extends MVCPortlet {
                 contributorAddress[0] = new InternetAddress(useremail);
                 subject = "Your submission to Climate-ADAPT";
                 body = "You have successfully submitted information to Climate-ADAPT regarding '" + measure.getName() + "'.\n";
-                body += "This material will be reviewed prior to publication. We thank you for your interest in Climate-ADAPT.";
+                body += "This material will be reviewed prior to publication.\n";
+                body += "As long as the item has not been approved you can still make changes by going to:\n\n";
+                body += hosturl + "/share-your-info/" ;
+                if (measure.getMao_type().equalsIgnoreCase("A")) {
+                    body += "case-studies";
+                } else {
+                    body += "adaptation-options";
+                }
+                body += "?submissionid=" + measure.getMeasureId();
+                body += "\n\nWe thank you for your interest in Climate-ADAPT.";
                 MailEngine.send(fromInternetAddress, contributorAddress, null, null, subject, body, false, null, null, null);
             } catch (Exception e) {
                 // do nothing
