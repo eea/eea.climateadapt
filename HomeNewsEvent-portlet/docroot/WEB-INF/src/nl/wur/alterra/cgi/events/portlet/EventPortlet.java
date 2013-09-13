f707b5bfbfe4aeb695718975a2be3e31e0be13fe
pitchmoh <pitchmoh@unknown.com>
2013-09-13 13:38:05 +0000

    Fix to Event portlet

--- HomeNewsEvent-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/events/portlet/EventPortlet.java
+++ HomeNewsEvent-portlet/docroot/WEB-INF/src/nl/wur/alterra/cgi/events/portlet/EventPortlet.java
@@ -1,7 +1,9 @@
 package nl.wur.alterra.cgi.events.portlet;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -58,20 +60,28 @@ public class EventPortlet extends MVCPortlet {
 			
 			String showParam = httpRequest.getParameter("show");
 			
-			Date displayDateGT = new Date();
-			Date displayDateLT = null;
+			Date displayDateGT = null;
+			Date displayDateLT = new Date();
 
 			int status = WorkflowConstants.STATUS_APPROVED;
 			Date reviewDate = null;
 			boolean andOperator = true;
-			int start = 0;
-			int end = ParamUtil.getInteger(renderRequest, "delta", 5);
+			int start = -1;
+			int end = -1;
 			
 			if (showParam != null && showParam.equalsIgnoreCase("full"))
 			{
 			   start = -1;
 			   end = -1;
-			   displayDateGT = null;
+			   displayDateLT = null;
+			}
+			
+			if (displayDateLT != null)
+			{
+			   Calendar c = Calendar.getInstance();
+			   c.setTime(displayDateLT);
+			   c.add(Calendar.DATE, 1);
+			   displayDateLT = c.getTime();
 			}
 			
 			OrderByComparator obc = new ArticleModifiedDateComparator(false);
@@ -80,21 +90,13 @@ public class EventPortlet extends MVCPortlet {
 				companyId, groupId, articleId, version, title, description, content,
 				type, structureIds, templateIds, displayDateGT, displayDateLT,
 				status, reviewDate, andOperator, start, end, obc);
-			
-			// we need to sort in ascending based on the display date
-			JournalComparator journalComparator = new JournalComparator(0);
-			Collections.sort(journalArticlesList, journalComparator);
-						
-			
-			List urlList = new ArrayList();
-			List journalList = new ArrayList();
-			List yearList = new ArrayList();
-			List monthList = new ArrayList();
-			List dayList = new ArrayList();
-			List locationList = new ArrayList();
-			
+		
+			List<JournalEvent> journalList = new ArrayList<JournalEvent>();
+		
+			int ctr = 0;
 			for (JournalArticle article : journalArticlesList)
 			{
+				ctr ++;
 				String name = "url"; // The name of the custom field
 				String urlValue = ""; // The value of the custom field: remember that this will always be a java.lang.String
 				
@@ -102,7 +104,6 @@ public class EventPortlet extends MVCPortlet {
 				Document document = SAXReaderUtil.read(article.getContentByLocale(article.getDefaultLocale()));
 				Node node = document.selectSingleNode("/root/dynamic-element[@name='" + name + "']/dynamic-content");
 				urlValue = node.getText();
-				
 				
 				name = "year";
 				String yearValue = "";
@@ -114,6 +115,7 @@ public class EventPortlet extends MVCPortlet {
 				String monthValue = "";
 				node = document.selectSingleNode("/root/dynamic-element[@name='" + name + "']/dynamic-content");
 				monthValue = node.getText();
+				String monthInMMFormat = monthValue.substring(0, 3);
 			
 				
 				name = "day";
@@ -127,30 +129,57 @@ public class EventPortlet extends MVCPortlet {
 				node = document.selectSingleNode("/root/dynamic-element[@name='" + name + "']/dynamic-content");
 				locationValue = node.getText();
 			
-				
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM/dd/yyyy");
+				String eventDate = monthInMMFormat + "/" + dayValue + "/" + yearValue;
+				Date eventDt = dateFormatter.parse(eventDate);
+			    Calendar c = Calendar.getInstance();
+			    c.setTime(new Date());
+			    c.set(Calendar.HOUR_OF_DAY, 0);
+			    c.set(Calendar.MINUTE, 0);
+			    c.set(Calendar.SECOND, 0);
+			    c.set(Calendar.MILLISECOND, 0);
+			    Date currentDt = c.getTime();
+			    
+			    boolean displayFlag = false;
+			    
+			    if (eventDt.compareTo(currentDt) >= 0 )
+			    {
+			    	displayFlag = true;
+			    }
+			      
 				
 				// show only the latest version
-				if(JournalArticleLocalServiceUtil.isLatestVersion(groupId,article.getArticleId(),article.getVersion())){
-					journalList.add(article);
-					urlList.add(urlValue);
-					yearList.add(yearValue);
-					monthList.add(monthValue);
-					dayList.add(dayValue);
-					locationList.add(locationValue);
+				if(JournalArticleLocalServiceUtil.isLatestVersion(groupId,article.getArticleId(),article.getVersion()) && displayFlag){
+					JournalEvent newEvent = new JournalEvent();
+					newEvent.setArticle(article);
+					newEvent.setDate(dayValue);
+					newEvent.setMonth(monthValue);
+					newEvent.setYear(yearValue);
+					newEvent.setUrl(urlValue);
+					newEvent.setLocation(locationValue);
+					newEvent.setEventDt(eventDt);
+					journalList.add(newEvent);
+				}
+			
+			}
+			
+			Collections.sort(journalList); 
+			// get the sublist for first 5 articles if it is not full page
+			if (showParam == null || ! showParam.equalsIgnoreCase("full"))
+			{
+				if (journalList.size() > 5)
+				{
+				   journalList = journalList.subList(0, 5);
 				}
 			}
 		
 			renderRequest.setAttribute("eventList", journalList);
-			renderRequest.setAttribute("urlList", urlList);
-			renderRequest.setAttribute("yearList", yearList);
-			renderRequest.setAttribute("monthList", monthList);
-			renderRequest.setAttribute("dayList", dayList);
-			renderRequest.setAttribute("locationList", locationList);
+			
 			if (showParam != null && showParam.equalsIgnoreCase("full"))
 			{
 				renderRequest.setAttribute("showall", "full");
 			}
-			
+		
 		}
         catch (Exception x) {
 			x.printStackTrace();
@@ -158,6 +187,76 @@ public class EventPortlet extends MVCPortlet {
 		}
 
 		super.doView(renderRequest, renderResponse);
+	}
+	
+	
+	public static class JournalEvent implements Comparable
+	{
+		String url;
+		String year;
+		String month;
+		String date;
+		String location;
+		Date eventDt;
+		JournalArticle article ;
+		
+		
+		public JournalArticle getArticle() {
+			return article;
+		}
+		public void setArticle(JournalArticle article) {
+			this.article = article;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public String getYear() {
+			return year;
+		}
+		public void setYear(String year) {
+			this.year = year;
+		}
+		public String getMonth() {
+			return month;
+		}
+		public void setMonth(String month) {
+			this.month = month;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+		public String getLocation() {
+			return location;
+		}
+		public void setLocation(String location) {
+			this.location = location;
+		}
+		public Date getEventDt() {
+			return eventDt;
+		}
+		public void setEventDt(Date eventDt) {
+			this.eventDt = eventDt;
+		}
+		
+		public int compareTo(Object o)
+		{
+			JournalEvent journal =(JournalEvent) o;
+			if (this.eventDt.compareTo(journal.getEventDt()) <= 0)
+			{
+				return -1;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+		
 	}
 	
 
