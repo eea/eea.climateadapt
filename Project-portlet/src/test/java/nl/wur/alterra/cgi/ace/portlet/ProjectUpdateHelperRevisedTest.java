6dea9a8f08d3eca5e48cd8244917cf1e9f4c5dd9
Søren Roug <soren.roug@eea.europa.eu>
2014-08-10 16:05:18 +0200

    Simple test of the static method escapeName()

---
+++ Project-portlet/src/test/java/nl/wur/alterra/cgi/ace/portlet/ProjectUpdateHelperRevisedTest.java
@@ -0,0 +1,19 @@
+package nl.wur.alterra.cgi.ace.portlet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProjectUpdateHelperRevisedTest {
+
+    /**
+     * There is an intention in escapeName to cut the string at 75 characters for the database,
+     * but due to a coding error the full string is returned.
+     */
+    @Test
+    public void simpleInit() {
+        String str = "X !$1234567890'(){[]}:,<=>\"#&?;*~ abcdefghijklmnopqrstuvwxyz Ηλέκτρα€/\\X";
+        String res = ProjectUpdateHelperRevised.escapeName(str);
+        //System.out.println(res);
+        Assert.assertEquals("X !$1234567890-()---------------- abcdefghijklmnopqrstuvwxyz Ηλέκτρα€--X", res);
+    }
+}
