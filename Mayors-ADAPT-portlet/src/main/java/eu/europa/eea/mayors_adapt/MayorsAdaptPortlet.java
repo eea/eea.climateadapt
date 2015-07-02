f4e7f1d917decac4128ef523ea833f8b894a8aae
gustavo.vazquez <gustavo.vazquez@chakray.com>
2015-07-02 15:13:13 +0200

    Mail buttons moved to City Profile Management page

--- Mayors-ADAPT-portlet/src/main/java/eu/europa/eea/mayors_adapt/MayorsAdaptPortlet.java
+++ Mayors-ADAPT-portlet/src/main/java/eu/europa/eea/mayors_adapt/MayorsAdaptPortlet.java
@@ -1,7 +1,10 @@
 package eu.europa.eea.mayors_adapt;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
@@ -9,10 +12,19 @@ import javax.portlet.RenderResponse;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
-import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.workflow.WorkflowInstance;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManagerUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTask;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+import com.liferay.portal.model.WorkflowInstanceLink;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.service.WorkflowInstanceLinkLocalServiceUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 
 public class MayorsAdaptPortlet extends LiferayPortlet {
 
@@ -24,6 +36,7 @@ public class MayorsAdaptPortlet extends LiferayPortlet {
 			RenderResponse renderResponse) throws IOException, PortletException {
 
 		include(viewTemplate, renderRequest, renderResponse);
+
 	}
 
 	protected void include(String path, RenderRequest renderRequest,
@@ -43,12 +56,59 @@ public class MayorsAdaptPortlet extends LiferayPortlet {
 	public void serveResource(ResourceRequest resourceRequest,
 			ResourceResponse resourceResponse) throws IOException,
 			PortletException {
-		// TODO Auto-generated method stub
 		super.serveResource(resourceRequest, resourceResponse);
 	}
 
-	public void saveArticle() {
-		System.out.println("Saving Article");
+	public void finishTask(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException {
+		String uuid = ParamUtil.getString(actionRequest, "token");
+		long groupId = ParamUtil.getLong(actionRequest, "groupId");
+		long companyId = ParamUtil.getLong(actionRequest, "companyId");
+
+
+		_log.info("Finishing Task for item: " + uuid);
+		try {
+			long userId = UserLocalServiceUtil.getUserByScreenName(companyId,
+					"cityprofilecontact").getUserId();
+			JournalArticle article = JournalArticleLocalServiceUtil
+					.getJournalArticleByUuidAndGroupId(uuid, groupId);
+			WorkflowInstanceLink workflowInstanceLink = WorkflowInstanceLinkLocalServiceUtil
+					.getWorkflowInstanceLink(article.getCompanyId(),
+							article.getGroupId(),
+							JournalArticle.class.getName(), article.getId());
+
+			WorkflowInstance workflowInstance = WorkflowInstanceManagerUtil
+					.getWorkflowInstance(article.getCompanyId(),
+							workflowInstanceLink.getWorkflowInstanceId());
+			_log.info("Gathering tasks for workflowInstance: "
+					+ workflowInstanceLink.getWorkflowInstanceId() + " user:"
+					+ userId + " companyId:"
+					+ workflowInstanceLink.getCompanyId());
+			List<WorkflowTask> tasks = WorkflowTaskManagerUtil
+					.getWorkflowTasksByWorkflowInstance(
+							workflowInstanceLink.getCompanyId(),
+							userId,
+							workflowInstanceLink.getWorkflowInstanceId(),
+							false, 0, 1, null);
+			long workflowTaskId = 0;
+
+			_log.info("Tasks found: " + tasks.size());
+
+			for (WorkflowTask task : tasks) {
+				_log.info("Task Found id: " + task.getWorkflowTaskId() + " "
+						+ task.getName());
+				workflowTaskId = task.getWorkflowTaskId();
+			}
+
+			WorkflowTaskManagerUtil.completeWorkflowTask(
+					workflowInstanceLink.getGroupId(),
+					workflowInstanceLink.getUserId(), workflowTaskId, "Finish",
+					null, workflowInstance.getWorkflowContext());
+			viewTemplate = "/ok.jsp";
+		} catch (Exception e) {
+			viewTemplate = "/ko.jsp";
+			e.printStackTrace();
+		}
 	}
 
 	protected String viewTemplate;
--- Mayors-ADAPT-portlet/src/main/webapp/view.jsp
+++ Mayors-ADAPT-portlet/src/main/webapp/view.jsp
@@ -1,3 +1,16 @@
+<%@page import="com.liferay.portal.kernel.log.Log"%>
+<%@page import="com.liferay.portal.kernel.log.LogFactoryUtil"%>
+<%@page import="com.liferay.portal.kernel.workflow.WorkflowTask"%>
+<%@page import="com.liferay.portal.kernel.util.ParamUtil"%>
+<%@page
+	import="com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil"%>
+<%@page import="java.util.List"%>
+<%@page
+	import="com.liferay.portal.kernel.workflow.WorkflowInstanceManagerUtil"%>
+<%@page import="com.liferay.portal.kernel.workflow.WorkflowInstance"%>
+<%@page
+	import="com.liferay.portal.service.WorkflowInstanceLinkLocalServiceUtil"%>
+<%@page import="com.liferay.portal.model.WorkflowInstanceLink"%>
 <%@page import="com.liferay.portal.kernel.util.LocaleUtil"%>
 <%@page import="com.liferay.util.Encryptor"%>
 <%@page import="com.liferay.portal.model.User"%>
@@ -14,17 +27,26 @@
 <%
 	HttpServletRequest httpReq = PortalUtil
 	.getOriginalServletRequest(request);
-	String articleId = httpReq.getParameter("articleId");
 	String uuid = httpReq.getParameter("token");
+
+	long groupId = ParamUtil.getLong(request, "groupId", 18L);
+	long companyId = ParamUtil.getLong(request, "companyId", 1L);
+	long userId = ParamUtil.getLong(request, "userId", UserLocalServiceUtil
+			.getUserByScreenName(companyId, "cityprofilecontact")
+			.getUserId());
 	
+	Log _log = LogFactoryUtil
+			.getLog("Mayors-ADAPT-portlet/view.jsp");
 	
 	JournalArticle article = JournalArticleLocalServiceUtil.getJournalArticleByUuidAndGroupId(uuid, layout.getGroupId());
 	request.setAttribute(WebKeys.JOURNAL_ARTICLE,article);
 	String cityName = article.getTitle(locale);
+	String articleId = article.getArticleId();
+	
 %>
 <h2>
 	Welcome to
-	<%=cityName %>
+	<%=cityName%>
 	city profile administration page
 </h2>
 Here you can manage Mayors-ADAPT initiative information relative to your
@@ -56,21 +78,38 @@ city.
 	<liferay-portlet:param name="showHeader" value="false" />
 </liferay-portlet:renderURL>
 
+<liferay-portlet:actionURL var="finishTaskURL" name="finishTask"
+	windowState="<%=LiferayWindowState.POP_UP.toString()%>">
+	<liferay-portlet:param name="token"
+		value="<%=uuid %>" />
+	<liferay-portlet:param name="companyId"
+		value="<%=String.valueOf(companyId)%>" />
+	<liferay-portlet:param name="groupId"
+		value="<%=String.valueOf(groupId)%>" />
+	<liferay-portlet:param name="userId" 
+		value="<%=String.valueOf(userId)%>" />
+</liferay-portlet:actionURL>
+
 <liferay-security:doAsURL doAsUserId="11270901"
 	var="impersonateArticleContentURL" />
 
 <aui:row>
 	<h3>Step 1 - Edit your City Profile</h3>
-	<aui:button value="Edit City Profile" onClick="edit()" />
+	<aui:button value="Edit" onClick="edit()" />
 </aui:row>
 <aui:row>
 	<h3>Step 2 - Preview your City Profile Mayors-ADAPT page</h3>
 	<aui:button value="Preview" onClick="preview()" />
 </aui:row>
-<!--<aui:row>
+<aui:row>
 	<h3>Step 3 - Finish your page and publish it</h3>
 	<aui:button value="Finish" onClick="finish()" />
-</aui:row>-->
+</aui:row>
+
+<aui:row>
+	<h3>Step 4 - Check your page in our Portal (after ours administrators approve it)</h3>
+	<aui:button value="Open"  onClick="check()" />
+</aui:row>
 
 <aui:script>
 	function preview() {
@@ -97,8 +136,22 @@ city.
 							uri : '<%= HtmlUtil.escapeJS(impersonateArticleContentURL.toString()) %>'
 						});
 	}
+	function finish() {
+		Liferay
+				.fire(
+						'previewArticle',
+						{
+							title : '<%= HtmlUtil.escapeJS(article.getTitle(locale)) %>',
+							uri : '<%= HtmlUtil.escapeJS(finishTaskURL.toString()) %>'
+						});
+	}
+	function check() {
+		window.open('/-/<%= HtmlUtil.escapeJS(cityName.toString())%>')
+	}
 </aui:script>
 	
+
+
 
 
 
